15cafeb103568e9c8c03dcd4c32af65708ef1922
unknown <nitzan@jama.com>
2021-05-20 18:02:46 +0300

    material-ui/core

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import MuiTheme from "./styles/MuiTheme";
+const theme = createMuiTheme(MuiTheme);
+
+const renderMethod = !!module.hot ? ReactDOM.render : ReactDOM.hydrate;
+
+renderMethod(
+   <MuiThemeProvider theme={theme}>
+      <CssBaseline>
+         <App />
+      </CssBaseline>
+   </MuiThemeProvider>,
+   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/styles/MuiTheme.js
@@ -0,0 +1,99 @@
+const font = "Varela Round, sans-serif";
+
+let MuiTheme = {
+   direction: "rtl",
+   fontFamily: font,
+   overrides: {
+      MuiCssBaseline: {
+         "@global": {
+            html: {
+               direction: "rtl",
+            },
+         },
+      },
+   },
+   // use this one
+   paletteFixed: {
+      primary: {
+         main: "#FF789B",
+         mainGradient: "#FF91AD",
+         pinkGradient: "linear-gradient(135deg, #ff91ad 0%, #ff789b 100%)",
+         overlay: "linear-gradient(180deg, rgba(248, 248, 248, 0) 0%, #f8f8f8 100%)",
+         dark: "#1A1A1A",
+         background: "#F8F8F8",
+         background2: "#FFFFFF",
+         textWeaker: "#9E9E9E",
+         someColor: "#f6f6fa",
+         subText: "#716e6e",
+         mainGradientColor1: "#FF91AD",
+         mainGradientColor2: "#FF789B",
+         disabled: "#A3A3A3",
+      },
+   },
+   shadow: {
+      card: "0px 4px 20px rgba(25, 25, 25, 0.1)",
+      pinkButton: "0px 4px 20px rgba(255, 148, 191, 0.2)",
+   },
+   sizes: {
+      header: 80,
+      bottomNav: 90,
+      card: {
+         image: 150,
+         video: 150,
+      },
+   },
+   margin: {
+      large: 20,
+      medium: 15,
+      main: 10,
+      small: 5,
+   },
+   padding: {
+      huge: 150,
+      extraLarge: 40,
+      large: 20,
+      main: 10,
+   },
+   borderRadius: {
+      small: 2,
+      main: 5,
+      large: 25,
+   },
+   border: {
+      grayed: "1px solid #dadada",
+   },
+   fonts: {
+      hugeTitle: 24,
+      largeTitle: 20,
+      title: 18,
+      subTitle: 16,
+      paragraph: 14,
+      subText: 12,
+   },
+   typography: {
+      fontFamily: '"Varela Round",Roboto,"Helvetica Neue",Arial,sans-serif',
+   },
+
+   // left for older components
+   palette: {
+      background: {
+         default: "#F8F8F8",
+      },
+      primary: {
+         main: "#FF789B",
+         white: "#ffffff",
+         dark: "#1A1A1A",
+         gray: "#F8F8F8",
+      },
+      secondary: {
+         main: "#fac0cf",
+         gray: "#f6f6fa",
+         dark: "#9E9E9E",
+      },
+      blue: {
+         main: "#499bea",
+      },
+   },
+};
+
+export default MuiTheme;
